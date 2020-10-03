ff559b451a7e237548851c2471c1ce6f11d73add
Mikulas Florek <mikulas.florek@gamedev.sk>
2020-10-03 23:40:17 +0200

    nodx option

--- genie.lua
+++ genie.lua
@@ -3,31 +3,39 @@ newoption {
 	description = "use dx12 backend"
 }
 
-project "renderer"
-	files { 
-		"src/**.c",
-		"src/**.cpp",
-		"src/**.h",
-		"genie.lua"
-	}
-	excludes { "../../src/renderer/gpu/gpu.cpp" }
+newoption {
+	trigger = "nodx",
+	description = "use dx12 backend"
+}
 
-	if _OPTIONS["dx12"] then
-		includedirs {"external/pix/Include/WinPixEventRuntime", "../../plugins/dx11/external/include/dx" }
-		files { "external/pix/bin/x64/WinPixEventRuntime.dll" }
-		copy { "external/pix/bin/x64/WinPixEventRuntime.dll" }
-		excludes { "src/gpu_dx.cpp" }
-		solution "LumixEngine"
-			defines { "LUMIX_DX12" }
-	else
-		excludes { "src/gpu_dx12.cpp" }
-		solution "LumixEngine"
-			defines { "LUMIX_DX11" }
+if _OPTIONS["nodx"] == nil then
+	project "renderer"
+
+		files { 
+			"src/**.c",
+			"src/**.cpp",
+			"src/**.h",
+			"genie.lua"
+		}
+		excludes { "../../src/renderer/gpu/gpu.cpp" }
+
+		if _OPTIONS["dx12"] then
+			includedirs {"external/pix/Include/WinPixEventRuntime", "../../plugins/dx11/external/include/dx" }
+			files { "external/pix/bin/x64/WinPixEventRuntime.dll" }
+			copy { "external/pix/bin/x64/WinPixEventRuntime.dll" }
+			excludes { "src/gpu_dx.cpp" }
+			solution "LumixEngine"
+				defines { "LUMIX_DX12" }
+		else
+			excludes { "src/gpu_dx12.cpp" }
+			solution "LumixEngine"
+				defines { "LUMIX_DX11" }
+		end
+
+
+	if build_studio then
+		project "studio"
+			libdirs { "external/lib/win64" .. "_" .. binary_api_dir .. "/release"}
+			libdirs { "external/pix/bin/x64" }
 	end
-
-
-if build_studio then
-	project "studio"
-		libdirs { "external/lib/win64" .. "_" .. binary_api_dir .. "/release"}
-		libdirs { "external/pix/bin/x64" }
 end
